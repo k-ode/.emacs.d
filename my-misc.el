f3e69f478e7e4e214f3e5043879c4518616ed421
Kim Grönqvist <kimgronqvist@gmail.com>
2014-02-27 10:59:51 +0100

    Add ibuffer-vc

--- my-misc.el
+++ my-misc.el
@@ -1,3 +1,21 @@
+(add-hook 'ibuffer-hook
+          (lambda ()
+            (ibuffer-vc-set-filter-groups-by-vc-root)
+            (unless (eq ibuffer-sorting-mode 'alphabetic)
+              (ibuffer-do-sort-by-alphabetic))))
+
+(setq ibuffer-formats
+      '((mark modified read-only vc-status-mini " "
+              (name 18 18 :left :elide)
+              " "
+              (size 9 -1 :right)
+              " "
+              (mode 16 16 :left :elide)
+              " "
+              (vc-status 16 16 :left)
+              " "
+              filename-and-process)))
+
 (require 'powershell-mode)
 
 (require 'powerline)
@@ -41,7 +59,7 @@
 
 ;; Change window quickly with S-left and S-right
 (when (fboundp 'windmove-default-keybindings)
-      (windmove-default-keybindings))
+  (windmove-default-keybindings))
 
 ;; EXTRACT THIS
 (defun my-csarhp-mode-fn ()
