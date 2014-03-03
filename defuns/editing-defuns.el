7b06d532bea63a1cc00ca354034fb832bf29ab12
Kim Grönqvist <kimgronqvist@gmail.com>
2014-03-03 23:08:49 +0100

    Try out some new stuff.

--- defuns/editing-defuns.el
+++ defuns/editing-defuns.el
@@ -92,3 +92,39 @@ the current position of point, then move it to the beginning of the line."
 (defun yank-unindented ()
   (interactive)
   (yank 1))
+
+(defun kill-region-or-backward-word ()
+  (interactive)
+  (if (region-active-p)
+      (kill-region (region-beginning) (region-end))
+    (backward-kill-word 1)))
+
+;; copy region if active
+;; otherwise copy to end of current line
+;;   * with prefix, copy N whole lines
+
+(defun copy-to-end-of-line ()
+  (interactive)
+  (kill-ring-save (point)
+                  (line-end-position))
+  (message "Copied to end of line"))
+
+(defun copy-whole-lines (arg)
+  "Copy lines (as many as prefix argument) in the kill ring"
+  (interactive "p")
+  (kill-ring-save (line-beginning-position)
+                  (line-beginning-position (+ 1 arg)))
+  (message "%d line%s copied" arg (if (= 1 arg) "" "s")))
+
+(defun copy-line (arg)
+  "Copy to end of line, or as many lines as prefix argument"
+  (interactive "P")
+  (if (null arg)
+      (copy-to-end-of-line)
+    (copy-whole-lines (prefix-numeric-value arg))))
+
+(defun save-region-or-current-line (arg)
+  (interactive "P")
+  (if (region-active-p)
+      (kill-ring-save (region-beginning) (region-end))
+    (copy-line arg)))
