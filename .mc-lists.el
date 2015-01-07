fc51d368fd91c900e9cc03c39760495ae27643f8
Kim Grönqvist <kimgronqvist@gmail.com>
2015-01-07 12:34:03 +0100

    Update multiple cursors and projectile settings

--- .mc-lists.el
+++ .mc-lists.el
@@ -9,6 +9,7 @@
         hippie-expand-no-case-fold
         js2-down-mouse-3
         js2-mode-show-node
+        js2r-kill
         kill-region
         mouse-save-then-kill
         multiple-cursors-mode
