7b99a3154bc94d5f6a96a6e1ca614523a5040075
Kim Grönqvist <kimgronqvist@gmail.com>
2015-05-07 11:57:08 +0200

    update

--- .mc-lists.el
+++ .mc-lists.el
@@ -7,6 +7,7 @@
         dired-next-line
         dired-previous-line
         electric-newline-and-maybe-indent
+        eval-expression
         hippie-expand-no-case-fold
         js2-down-mouse-3
         js2-mode-show-node
@@ -14,6 +15,9 @@
         kill-region
         mouse-save-then-kill
         multiple-cursors-mode
+        org-beginning-of-line
+        org-end-of-line
+        org-self-insert-command
         save-region-or-current-line
         sgml-delete-tag
         skip-to-next-blank-line
@@ -27,6 +31,7 @@
         tagedit-insert-gt
         tagedit-insert-lt
         tagedit-kill
+        tagedit-kill-attribute
         wdired-finish-edit
         wgrep-finish-edit
         yank-rectangle
