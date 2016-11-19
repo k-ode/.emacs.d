b33c5bf0557ca7cc9effd97b56e131c4fb9f4fae
Kim Grönqvist <kg@conrab.se>
2016-11-19 19:08:03 +0100

    always set bash as default shell

--- user-custom/setup-shell.el
+++ user-custom/setup-shell.el
@@ -1,11 +1,9 @@
-(defun kg-set-bash-shell ()
-  (interactive)
-  (when window-system
-    (setq explicit-shell-file-name "C:/Program Files/Git/bin/sh.exe")
-    (setq shell-file-name explicit-shell-file-name)
-    (setq explicit-sh.exe-args '("--login" "-i"))
-    (setenv "SHELL" shell-file-name)
-    (add-to-list 'exec-path "C:/Program Files/Git/bin")
-    (add-hook 'comint-output-filter-functions 'comint-strip-ctrl-m)))
+(when window-system
+  (setq explicit-shell-file-name "C:/Program Files/Git/bin/sh.exe")
+  (setq shell-file-name explicit-shell-file-name)
+  (setq explicit-sh.exe-args '("--login" "-i"))
+  (setenv "SHELL" shell-file-name)
+  (add-to-list 'exec-path "C:/Program Files/Git/bin")
+  (add-hook 'comint-output-filter-functions 'comint-strip-ctrl-m))
 
 (provide 'setup-shell)
